80469884937953899f3d2a857edfd81c29adcb2d
Vince Carney <vincecarney@gmail.com>
2014-04-18 11:59:32 -0500

    Dev updates

---
+++ project/custom/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+from custom_user.models import AbstractEmailUser
+
+
+class Department(models.Model):
+    title = models.CharField(max_length=256)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return u'%s' % self.title
+
+
+class User(AbstractEmailUser):
+    """
+    EmailUser with custom fields
+    """
+    first_name = models.CharField(max_length=128, blank=True)
+    last_name = models.CharField(max_length=128, blank=True)
+    department = models.ForeignKey(Department, blank=True, null=True)
+
+    def __unicode__(self):
+        return u'%s' % self.email
