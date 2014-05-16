6a0e7e8106c162b3584d074f2b5e824c143b47f1
Vince Carney <vincecarney@gmail.com>
2014-05-16 10:07:59 -0500

    LDAP department

--- project/waterauth/backends.py
+++ project/waterauth/backends.py
@@ -3,6 +3,8 @@ import ldap
 from django.contrib.auth import get_user_model
 from django.conf import settings
 
+from custom.models import Department
+
 
 class LDAPBackend(object):
     """
@@ -31,6 +33,20 @@ class LDAPBackend(object):
             last_name = ldap_search[0][1]['sn'][0]
             email = ldap_search[0][1]['mail'][0]
 
+            try:
+                ldap_department = ldap_search[0][1]['department'][0]
+            except:
+                ldap_department = None
+
+            if ldap_department:
+                try:
+                    department = Department.objects.get(title=ldap_department)
+                except Department.DoesNotExist:
+                    department = Department(
+                        title=ldap_department,
+                    )
+                    department.save()
+
             User = get_user_model()
 
             try:
@@ -38,11 +54,18 @@ class LDAPBackend(object):
                 user.first_name = first_name
                 user.last_name = last_name
                 user.email = email.lower()
+                if ldap_department:
+                    user.department = department
                 user.set_unusable_password()
                 user.save()
             except User.DoesNotExist:
-                user = User(first_name=first_name, last_name=last_name,
-                            email=email.lower())
+                user = User(
+                    first_name=first_name,
+                    last_name=last_name,
+                    email=email.lower(),
+                )
+                if ldap_department:
+                    user.department = department
                 user.set_unusable_password()
                 user.save()
             conn.unbind()
